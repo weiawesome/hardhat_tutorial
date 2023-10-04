dee66ef081b3b197293dac4d2eddc1ac1748427f
tcweeei <open891013@gmail.com>
2023-10-04 19:45:39 +0800

    Add the network setting of ganache.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,14 @@
+// @ts-ignore
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    ganache: {
+      url: "http://localhost:7545",
+    },
+  },
 };
 
 export default config;
